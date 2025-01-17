077e38d3a15e23f8509bfe0c6b40bc8548494242
Saad Badat <saadbadat@Saads-MBP-34.attlocal.net>
2025-01-17 05:37:06 -0500

    bug

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "https://75d9-2600-381-cfa0-fa0-8840-250a-1a3e-8a52.ngrok-free.app/predict",
+        "https://d35a-2600-381-cfa0-fa0-8840-250a-1a3e-8a52.ngrok-free.app/predict",
         formData,
         {
           headers: {
